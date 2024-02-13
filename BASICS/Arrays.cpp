42754e9ea6c18da7cd5f8b4765da5140a24f4c7f
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-02-13 14:59:04 +0530

    adding array and input output txt files

---
+++ BASICS/Arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    cout << arr[2];
+    return 0;
+}
