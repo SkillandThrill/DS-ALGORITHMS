4dd2545fe5255b2250352da23af54193614062c1
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-02-14 15:49:16 +0530

    updating and adding all the files in Repo learned the functions and pass by reference and values in funcitons

--- BASICS/functions.cpp
+++ BASICS/functions.cpp
@@ -17,6 +17,37 @@ void printName(string name)
     cout << "Hey " << name << endl;
 }
 */
+
+// int sum(int num1, int num2)
+// {
+//     int num3 = num1 + num2;
+//     return num3;
+// }
+
+// pass by values;
+
+// void doSomething(int num)
+// {
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+// }
+
+// pass by reference
+
+// void doSomething(string s)
+// {
+//     s[0] = 't';
+//     cout << s << endl;
+// }
+
+void doSomething(int arr[], int n)
+{
+    arr[0] += 100;
+    cout << "value inside function :" << arr[0] << endl;
+}
 int main()
 {
 
@@ -39,10 +70,56 @@ int main()
         */
 
     // Take 2 numbers and print its sum
+    /*
+        int num1, num2;
+        cin >> num1 >> num2;
+        int res = sum(num1, num2);
+        cout << res;
+    */
 
-    int num1, num2;
-    cin >> num1 >> num2;
-    int num3 = num1 + num2;
-    cout << num3 << endl;
+    /*    int num1, num2;
+
+     cin >> num1 >> num2;
+     int minimum = max(num1, num2);
+     cout << minimum;
+     */
+
+    // int num = 10;
+    // doSomething(num);
+    // cout << num << endl;
+
+    // string s = "Gaurav";
+    // doSomething(s);
+    // cout << s << endl;
+
+    // int arr[5];
+
+    // for (int i = 0; i <= 4; i = i + 1)
+    // {
+    //     cin >> arr[i];
+    // }
+    // for (int i = 0; i <= 4; i = i + 1)
+    // {
+    //     cout << arr[i] << endl;
+    // }
+
+    // example of array in pass by reference
+
+    int n = 5;
+    int arr[n];
+    for (int i = 0; i < n; i = i + 1)
+    {
+        cin >> arr[i];
+    }
+
+    doSomething(arr, n);
+    for (int i = 0; i <= n; i = i + 1)
+    {
+        cin >> arr[i];
+    }
+
+    doSomething(arr, n);
+
+    cout << "value inside  int  main : " << arr[0] << endl;
     return 0;
 }
---
+++ BASICS/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+
+    int arr[5];
+
+    for (int i = 0; i <= 4; i = i + 1)
+    {
+        cout << i << endl;
+        cin >> arr[i];
+    }
+    return 0;
+}
