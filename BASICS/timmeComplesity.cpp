9d26a86d8f56228b930c8bcfbf9b9002febb4e7f
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-02-14 16:39:11 +0530

    upd time complexity file on the repo

---
+++ BASICS/timmeComplesity.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // for (int i = 1; i <= 5; i = i + 1)
+    // {
+
+    //     cout << "Gaurav" << endl;
+    // }
+
+    for (int i = 0; i <= 4; i = i + 1)
+    {
+        for (int j = 0; j < 4; j = j + 1)
+        {
+            cout << "";
+        }
+    }
+
+    return 0;
+}
